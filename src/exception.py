93db51bf55028d99ef7a166bb53d8c48b6ee1e8c
Asmit Das <dasasmit123@gmail.com>
2026-05-19 01:20:29 +0530

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,33 @@
+import sys
+import logging
+
+def error_messege_detail(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    file_name= exc_tb.tb_frame.f_code.co_filename
+    error_messege = "Error occured in python script name[{0}] line number [{1}] error messege[{2}]".format(
+      file_name,exc_tb.tb_lineno,str(error)
+        
+     
+
+    )
+    return error_messege
+    
+    
+
+
+class CustomException(Exception):
+    def __init__(self,error_messege,error_detail:sys):
+        super().__init__(error_messege)
+        self.error_messege= error_messege_detail(error_messege,error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_messege
+    
+
+
+if __name__ =="__main__":
+    try:
+        a= 1/0
+    except Exception as e:
+        logging.info("Divide by zero")
+        raise CustomException(e,sys)
